7ebca148d4b8ee0392b30135554228bc18fe2896
PriyankaMuthu <132463693+PriyankaMuthu@users.noreply.github.com>
2024-07-07 11:44:05 +0530

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@ const photos = [
     { src: 'img14.jpg', message: 'and there are days.....i wanted to come college to see those...' },
     { src: 'img13.jpg', message: 'even everything around u is not good....but you around me is the best.....'},
     { src: 'img1.jpg', message: 'Now.....what is the karuthu i am saying is ......'},
-    { src: 'img16.jpg', message: 'happpiieeeeee......porandhanaaaallllllll.......Dharshuuuuuu 💙💖 '},
+    { src: 'img16.jpg', message: 'happpiieeeeee...porandhanaaaallllllll...Dharshuuuuuu💙💖'},
     { src: 'img18.jpg', message: 'Have a best year ahead.....I am always with you.....Hope the god brings thousands of blessings to you on this day!!!!!......😊'}
   ];
   
